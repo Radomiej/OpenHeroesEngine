f2894364c8e638f2bd13d8ca7f3f9c650848596c
Radomiej <radek1993-91@go2.pl>
2020-03-18 15:23:48 +0100

    Add fraction system: resource adder.

---
+++ OpenHeroesEngine/WorldMap/Systems/FactionSystem.cs
@@ -0,0 +1,30 @@
+﻿using Artemis.Attributes;
+using Artemis.Manager;
+using OpenHeroesEngine.Artemis;
+using OpenHeroesEngine.WorldMap.Components;
+using OpenHeroesEngine.WorldMap.Events;
+using OpenHeroesEngine.WorldMap.Models;
+using Radomiej.JavityBus;
+
+namespace OpenHeroesEngine.WorldMap.Systems
+{
+    [ArtemisEntitySystem(GameLoopType = GameLoopType.Update)]
+    public class FactionSystem : EventBasedSystem
+    {
+        [Subscribe]
+        public void AddResourceListener(AddResourceToFractionEvent addResourceToFractionEvent)
+        {
+            if(addResourceToFractionEvent.Fraction == null) return;
+            Fraction fraction = addResourceToFractionEvent.Fraction;
+            Resource resource = addResourceToFractionEvent.Resource;
+
+            if (!fraction.Resources.ContainsKey(resource.Definition.Name))
+            {
+                Resource fractionResource = new Resource(resource.Definition, 0);
+                fraction.Resources.Add(resource.Definition.Name, fractionResource);
+            }
+
+            fraction.Resources[resource.Definition.Name].Amount += resource.Amount;
+        }
+    }
+}
